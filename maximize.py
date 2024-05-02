b8663f07b3cdd2801d249b8e788cf02790902cb9
Vijay6923 <kumarviju2468@gmail.com>
2024-05-02 22:07:29 +0530

    codeforces

---
+++ maximize.py
@@ -0,0 +1,20 @@
+t = int(input())  
+for _ in range(t):
+    n, m = map(int, input().split())  
+    a = input()  
+    b = input()  
+    i = 0  
+    j = 0 
+    
+    
+    while i < n and j < m:
+       
+        if a[i] == b[j]:
+            i += 1
+            j += 1
+        else:
+            
+            j += 1
+    
+    
+    print(i)
