87cae288235146c677cba6aec4639c7f6ad78e98
Vijay6923 <kumarviju2468@gmail.com>
2024-05-29 20:58:01 +0530

    codeforces

---
+++ football_codeforces.py
@@ -0,0 +1,14 @@
+def determine_winner(n, goals):
+    goal_count = {}
+    
+    for team in goals:
+        if team in goal_count:
+            goal_count[team] += 1
+        else:
+            goal_count[team] = 1
+    winner = max(goal_count, key=goal_count.get)
+    return winner
+n = int(input())
+goals = [input().strip() for _ in range(n)]
+winner = determine_winner(n, goals)
+print(winner)
