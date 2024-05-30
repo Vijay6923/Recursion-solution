35783a894339c0a706adbab636c057c3fb95de70
Vijay6923 <kumarviju2468@gmail.com>
2024-05-30 23:32:21 +0530

    codeforces

---
+++ divisor_game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0
+
+
+        if divisorGame(n):
+            print("Alice wins")
+        else:
+            print("Bob wins")
+
+            
