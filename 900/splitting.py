b529130e6df741d5e85e33fa79b97ac9a391d150
Vijay6923 <kumarviju2468@gmail.com>
2024-06-14 23:36:03 +0530

    codeforces

--- 900/splitting.py
+++ 900/splitting.py
@@ -1,23 +1,17 @@
-def solve(test_cases):
-    results = []
-    for case in test_cases:
-        n, array = case
-        if n < 3:
-            results.append("NO")
+def main():
+    T = int(input())
+    
+    for _ in range(T):
+        n = int(input())
+        a = list(map(int, input().split()))
+        
+        if a[0] == a[-1]:
+            print("NO")
         else:
-            results.append("YES")
-          
-            results.append("R" + "B" * (n - 1))
-    return results
- 
-t = int(input())
-test_cases = []
-for _ in range(t):
-    n = int(input())
-    array = list(map(int, input().split()))
-    test_cases.append((n, array))
- 
-results = solve(test_cases)
- 
-for result in results:
-    print(result)
+            print("YES")
+            s = ['R'] * n
+            s[1] = 'B'
+            print(''.join(s))
+
+if __name__ == "__main__":
+    main()
