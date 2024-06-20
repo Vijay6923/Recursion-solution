d4fe68b27f407cf6518990d05468acfaafb38e1e
Vijay6923 <kumarviju2468@gmail.com>
2024-06-20 13:27:50 +0530

    codeforces

--- 900/card_construction.py
+++ 900/card_construction.py
@@ -1,19 +1,51 @@
-t = int(input())
-cards = []
-h = 1
-while True:
-    required_cards = h * (3 * h + 1) // 2
-    if required_cards > 10**9:
-        break
-    cards.append(required_cards)
-    h += 1
-
-for _ in range(t):
+def solve():
     n = int(input())
-    count = 0
+    cnt = 0
+    
+    while n > 2:
+        t = 5
+        now = 2
+        while n >= t + now:
+            now = t + now
+            t += 3
+        n -= now
+        cnt += 1
+    
+    if n >= 2:
+        cnt += 1
+    
+    print(cnt)
 
-    for i in reversed(range(len(cards))):
-        count += n // cards[i]
-        n %= cards[i]
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    testcase = int(data[0])
+    index = 1
+    
+    results = []
+    for _ in range(testcase):
+        n = int(data[index])
+        index += 1
+        cnt = 0
+        
+        while n > 2:
+            t = 5
+            now = 2
+            while n >= t + now:
+                now = t + now
+                t += 3
+            n -= now
+            cnt += 1
+        
+        if n >= 2:
+            cnt += 1
+        
+        results.append(cnt)
+    
+    for result in results:
+        print(result)
 
-    print(count)
+if __name__ == "__main__":
+    main()
