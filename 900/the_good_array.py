1f8a4a613dd662de2173612779bb804faff265ac
Vijay6923 <kumarviju2468@gmail.com>
2024-06-28 16:02:02 +0530

    codeforces

--- 900/the_good_array.py
+++ 900/the_good_array.py
@@ -1,16 +1,37 @@
-import math
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    n = int(data[0])
+    a = list(map(int, data[1:]))
+    
+    maxx = 0
+    maxx2 = 0
+    total = 0
+    mem = []
+   
+    for j in a:
+        total += j
+        if j > maxx:
+            maxx2 = maxx
+            maxx = j
+        elif j > maxx2:
+            maxx2 = j
+    
+    checknice = 0
+    for i in range(n):
+        if a[i] != maxx:
+            if maxx == (total - a[i] - maxx):
+                mem.append(i + 1)
+                checknice += 1
+        else:
+            if maxx2 == (total - a[i] - maxx2):
+                mem.append(i + 1)
+                checknice += 1
+    
+    print(checknice)
+    print(' '.join(map(str, mem)))
 
-t = int(input().strip())
-
-for _ in range(t):
-    n, k = map(int, input().strip().split())
-    
-    min_ones = float('inf')
-    
-    for i in range(1, n + 1):
-        start_ones = math.ceil(i / k)
-        end_ones = math.ceil((n - i + 1) / k)
-        total_ones = start_ones + end_ones
-        min_ones = min(min_ones, total_ones)
-    
-    print(min_ones)
+if __name__ == "__main__":
+    main()
